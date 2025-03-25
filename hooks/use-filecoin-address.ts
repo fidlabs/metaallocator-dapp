66f93275306da5045412cc22822097a952712ec3
Maciej Trojan <maciej.trojan@neti-soft.com>
2025-03-25 10:27:39 +0100

    chore: remove console logs

--- hooks/use-filecoin-address.ts
+++ hooks/use-filecoin-address.ts
@@ -3,8 +3,8 @@ import { FilecoinAddress } from "@/types/common";
 import { QueryFunction, useQuery } from "@tanstack/react-query";
 import { useCallback } from "react";
 import { type Address } from "viem";
-import { useFilecoinPublicClient } from "./use-filecoin-public-client";
 import { useChainId } from "wagmi";
+import { useFilecoinPublicClient } from "./use-filecoin-public-client";
 
 type UseFilecoinAddressQueryKey = [
   QueryKey.FILECOIN_ADDRESS,
@@ -31,49 +31,38 @@ export function useFilecoinAddress({
     QueryFunction<FilecoinAddress, UseFilecoinAddressQueryKey>
   >(
     async ({ queryKey }) => {
-      try {
-        if (!filecoinClient) {
-          throw new Error("Filecoin client not available");
-        }
-
-        const [, , ethAddress, robust] = queryKey;
-        const maybeFilecoinAddress = await filecoinClient.request({
-          method: "Filecoin.EthAddressToFilecoinAddress",
-          params: [ethAddress],
-        });
-
-        if (maybeFilecoinAddress === null) {
-          throw new Error(
-            `Could not convert ETH address "${ethAddress} to Filecoin address."`
-          );
-        }
-
-        console.log("NOT ROBUST", maybeFilecoinAddress);
-
-        if (!robust || !f0AddressRegex.test(maybeFilecoinAddress)) {
-          return maybeFilecoinAddress;
-        }
-
-        console.log(`FETCHING ROBUST FOR ${maybeFilecoinAddress}`);
-
-        const maybeFilecoinRobustAddress = await filecoinClient.request({
-          method: "Filecoin.StateLookupRobustAddress",
-          params: [maybeFilecoinAddress, null],
-        });
-
-        console.log("ROBUST", maybeFilecoinAddress, maybeFilecoinRobustAddress);
-
-        if (maybeFilecoinRobustAddress === null) {
-          throw new Error(
-            `Could not convert ETH address "${ethAddress} to Filecoin address."`
-          );
-        }
-
-        return maybeFilecoinRobustAddress;
-      } catch (error) {
-        console.warn("ERROR FETCHING FILECOIN ADDRESS", error);
-        throw error;
+      if (!filecoinClient) {
+        throw new Error("Filecoin client not available");
       }
+
+      const [, , ethAddress, robust] = queryKey;
+      const maybeFilecoinAddress = await filecoinClient.request({
+        method: "Filecoin.EthAddressToFilecoinAddress",
+        params: [ethAddress],
+      });
+
+      if (maybeFilecoinAddress === null) {
+        throw new Error(
+          `Could not convert ETH address "${ethAddress} to Filecoin address."`
+        );
+      }
+
+      if (!robust || !f0AddressRegex.test(maybeFilecoinAddress)) {
+        return maybeFilecoinAddress;
+      }
+
+      const maybeFilecoinRobustAddress = await filecoinClient.request({
+        method: "Filecoin.StateLookupRobustAddress",
+        params: [maybeFilecoinAddress, null],
+      });
+
+      if (maybeFilecoinRobustAddress === null) {
+        throw new Error(
+          `Could not convert ETH address "${ethAddress} to Filecoin address."`
+        );
+      }
+
+      return maybeFilecoinRobustAddress;
     },
     [filecoinClient]
   );
