12d0914cfa647284175bf6f29eea049517ce194f
tmaciej-neti <maciej.trojan@neti-soft.com>
2025-11-13 10:44:28 +0100

    feat: add SLA request datacap widget

--- app/(app)/sla/page.tsx
+++ app/(app)/sla/page.tsx
@@ -2,8 +2,11 @@ import ScreenBreadcrumbs from "@/components/ScreenBreadcrumbs";
 import { BeneficiaryContractDeployWidget } from "./components/beneficiary-contract-deploy-widget";
 import { RegisterSLAWidget } from "./components/register-sla-widget";
 import { type Address } from "viem";
+import { RequestDatacapWidget } from "./components/request-datacap-widget";
 
 const factoryAddress: Address = "0x4d239cD2c62475BEa41e09BACBe59a9380C28220";
+const slaAllocatorAddress: Address =
+  "0x4Edca39825772c21739272F9212987f92b36e40E";
 const slaRegistryAddress: Address =
   "0xadE4feEB2f4b6D829765B430c1a8674E1607762E";
 
@@ -25,6 +28,10 @@ export default function SLAPage() {
 
       <div className="flex flex-col gap-8">
         <RegisterSLAWidget slaRegistryAddress={slaRegistryAddress} />
+        <RequestDatacapWidget
+          slaAllocatorAddress={slaAllocatorAddress}
+          slaRegistryAddress={slaRegistryAddress}
+        />
         <BeneficiaryContractDeployWidget factoryAddress={factoryAddress} />
       </div>
     </div>
